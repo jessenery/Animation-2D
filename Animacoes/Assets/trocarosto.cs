43c7f7c69ec5e106985b10fa3041201701320ade
Jesse Nery Filho <jessenery@hotmail.com>
2014-10-24 13:04:36 -0200

    no message

---
+++ Animacoes/Assets/trocarosto.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trocarosto : MonoBehaviour {
+
+	public GameObject player;
+	private SpriteRenderer spri;
+	public Sprite[] texturas;
+	private int i=0;
+
+	// Use this for initialization
+	void Start () {
+		spri=player.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.D)){
+			print(i%texturas.Length);
+			i++;
+			spri.sprite = texturas[i%texturas.Length];
+
+		}
+	
+	}
+}
